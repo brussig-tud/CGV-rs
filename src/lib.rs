75d1a03a363873f3127855161dac3fb107eabacb
Benjamin Russig <benjamin.russig@tu-dresden.de>
2024-10-11 14:03:44 +0200

    Sanitize surface creation and window resizing

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,11 @@
 // Language config
 //
 
-// Fuck this convention. Probably the worst aspect of Rust after the lack of a standardized ABI
+// Eff this convention. Probably the worst aspect of Rust after the lack of a standardized ABI
 #![allow(non_snake_case)]
+
+// Eff this "feature" as well.
+#[allow(unused_must_use)]
 
 
 
@@ -60,16 +63,22 @@ pub enum UserEvent {
 
 pub struct App {
 	state: Option<state::State>,
-	event_loop_proxy: EventLoopProxy<UserEvent>,
-	redrawOnceOnWait: bool
+	eventLoopProxy: EventLoopProxy<UserEvent>,
+	redrawOnceOnWait: bool,
+
+	#[cfg(target_arch = "wasm32")]
+	canvas: Option<web_sys::Element>
 }
 
 impl App {
 	pub fn new (event_loop: &EventLoop<UserEvent>) -> Self {
 		Self {
 			state: None,
-			event_loop_proxy: event_loop.create_proxy(),
-			redrawOnceOnWait: false
+			eventLoopProxy: event_loop.create_proxy(),
+			redrawOnceOnWait: false,
+
+			#[cfg(target_arch = "wasm32")]
+			canvas: None
 		}
 	}
 }
@@ -92,29 +101,25 @@ impl ApplicationHandler<UserEvent> for App
 
 		#[cfg(target_arch = "wasm32")] {
 			use web_sys::Element;
-			use winit::{dpi::PhysicalSize, platform::web::WindowExtWebSys};
+			use winit::platform::web::WindowExtWebSys;
 
 			web_sys::window()
 				.and_then(|win| win.document())
 				.and_then(|doc| {
-					let canvas = Element::from(window.canvas()?);
-					canvas.set_attribute("style", "width:100% !important; height:100% !important").unwrap();
+					self.canvas = Some(Element::from(window.canvas()?));
+					let canvas = self.canvas.as_ref().unwrap();
 					doc.body()?.append_child(&canvas).ok()?;
 					Some(())
 				})
 				.expect("Couldn't append canvas to document body.");
-
-			// Winit prevents sizing with CSS, so we have to set
-			// the size manually when on web.
-			//let _ = window.request_inner_size(PhysicalSize::new(450, 400));
 		}
 
 		#[cfg(target_arch = "wasm32")] {
 			let state_future = state::State::new(window);
-			let event_loop_proxy = self.event_loop_proxy.clone();
+			let eventLoopProxy = self.eventLoopProxy.clone();
 			let future = async move {
 				let state = state_future.await;
-				assert!(event_loop_proxy
+				assert!(eventLoopProxy
 					.send_event(UserEvent::StateReady(state))
 					.is_ok());
 			};
@@ -123,21 +128,31 @@ impl ApplicationHandler<UserEvent> for App
 		#[cfg(not(target_arch = "wasm32"))] {
 			let state = pollster::block_on(state::State::new(window));
 			assert!(self
-				.event_loop_proxy
+				.eventLoopProxy
 				.send_event(UserEvent::StateReady(state))
 				.is_ok());
 		}
 	}
 
-	fn user_event (&mut self, _: &ActiveEventLoop, event: UserEvent) {
+	/// The user event hook - for now, only used to commit a new (asynchronously initialized) application state.
+	fn user_event (&mut self, _: &ActiveEventLoop, event: UserEvent)
+	{
+		// Apply newly initialized state
 		tracing::info!("Application state ready.");
 		let UserEvent::StateReady(state) = event;
 		self.state = Some(state);
+
+		// WASM, for some reason, needs a resize event for the main surface to become fully
+		// configured. Since we need to hook up the size of the canvas hosting the surface to the
+		// browser window anyway, this is a good opportunity for dispatching that initial resize.
+		#[cfg(target_arch = "wasm32")]
+		self.canvas.as_ref().unwrap().set_attribute(
+			"style", "width:100% !important; height:100% !important"
+		).unwrap();
 	}
 
-	fn window_event(
-		&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent
-	){
+	fn window_event (&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent)
+	{
 		match event
 		{
 			// Main window resize
@@ -145,7 +160,7 @@ impl ApplicationHandler<UserEvent> for App
 				if let Some(state) = self.state.as_mut() {
 					state.resize(physical_size);
 				}
-				#[cfg(not(target_arch="wasm32"))]{
+				#[cfg(not(target_arch="wasm32"))] {
 					self.redrawOnceOnWait = true
 				}
 			}
@@ -195,7 +210,8 @@ impl ApplicationHandler<UserEvent> for App
 		}
 	}
 
-	fn about_to_wait (&mut self, _: &ActiveEventLoop) {
+	fn about_to_wait (&mut self, _: &ActiveEventLoop)
+	{
 		if let Some(ref state) = self.state {
 			if self.redrawOnceOnWait {
 				self.redrawOnceOnWait = false;
@@ -225,15 +241,16 @@ pub fn run() -> Result<()>
 	#[cfg(debug_assertions)] {
 		envFilterBuilder = envFilterBuilder.with_default_directive(tracing::Level::DEBUG.into());
 	}
+
 	#[cfg(not(debug_assertions))] {
 		envFilterBuilder = envFilterBuilder.with_default_directive(tracing::Level::INFO.into());
 	}
+
 	let env_filter = envFilterBuilder
 		.from_env_lossy()
 		.add_directive("wgpu_core::device::resource=warn".parse()?);
 	let subscriber = tracing_subscriber::registry().with(env_filter);
-	#[cfg(target_arch = "wasm32")]
-	{
+	#[cfg(target_arch = "wasm32")] {
 		use tracing_wasm::{WASMLayer, WASMLayerConfig};
 
 		console_error_panic_hook::set_once();
@@ -241,11 +258,11 @@ pub fn run() -> Result<()>
 
 		subscriber.with(wasm_layer).init();
 	}
-	#[cfg(not(target_arch = "wasm32"))]
-	{
+	#[cfg(not(target_arch = "wasm32"))] {
 		let fmt_layer = tracing_subscriber::fmt::Layer::default();
 		subscriber.with(fmt_layer).init();
 	}
+
 	tracing::info!("Starting...");
 
 	let eventLoop = EventLoop::<UserEvent>::with_user_event().build()?;
