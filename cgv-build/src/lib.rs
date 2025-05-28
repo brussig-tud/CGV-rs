69ffc3b3430de7002542c93709a02841fbf20583
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2025-05-28 17:09:06 +0200

    Make debugging the build scripts a Cargo feature

--- cgv-build/src/lib.rs
+++ cgv-build/src/lib.rs
@@ -4,6 +4,10 @@
 // Language config
 //
 
+// Allow debugging the build script
+#![allow(internal_features)]
+#![feature(core_intrinsics)]
+
 // Eff this convention.
 #![allow(non_snake_case)]
 
@@ -152,6 +156,26 @@ pub fn getScriptStartTime () -> std::time::SystemTime {
 	*SCRIPT_START_TIME
 }
 
+/// Attach VS Code debugger to the build process, optionally halting execution right after
+pub fn debugWithVsCode (halt: bool) -> Result<()>
+{
+	let url = format!(
+		"vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','pid':{}}}", std::process::id()
+	);
+	match std::process::Command::new("code").arg("--open-url").arg(url).output()
+	{
+		Ok(output) => {
+			if output.status.success() {
+				std::thread::sleep(std::time::Duration::from_secs(3)); // <- give debugger time to attach
+				std::intrinsics::breakpoint();
+				Ok(())
+			} else {
+				Err(anyhow!("Could not attach debugger to build process: {}", String::from_utf8_lossy(&output.stderr)))
+			}
+		}
+		Err(err) => Err(anyhow!("Could not attach debugger to build process: {}", err))
+	}
+}
 /// Report the filename used for storing [Setup](build setups)..
 pub fn getSetupFilename () -> &'static str {
 	"_CGV_BUILD_SETUP"
--- cgv-sample/build.rs
+++ cgv-sample/build.rs
@@ -1,7 +1,14 @@
 
-/// Custom build steps – apply the build setup as passed on from *CGV-rs* and support WASM deployment.
+/// Custom build steps – set up build script debugging, apply the build setup as passed on from *CGV-rs*, compile
+/// shaders and support WASM deployment.
 fn main() -> cgv_build::Result<()>
 {
+	// Support debugging this build script (currently hard-wired to VS Code until IDEs start providing proper means of
+	// build script debugging)
+	if std::env::var("CARGO_FEATURE_BUILD_SCRIPT_DEBUG").is_ok() {
+		cgv_build::debugWithVsCode(true)?;
+	}
+
 	// Apply CGV-rs build setup
 	cgv_build::applyBuildSetup()?;
 
