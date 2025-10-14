4a5b8d079c5179d70c86756e82f7f903424a9619
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2025-10-14 19:01:16 +0200

    [shader] Prepare for proper, actually usable compilation environments

--- cgv-shader/src/compile/environment.rs
+++ cgv-shader/src/compile/environment.rs
@@ -5,11 +5,13 @@
 //
 
 // Standard library
-use std::{vec::Vec, collections::BTreeSet, collections::BTreeMap, error::Error, fmt::{Display, Formatter}};
+use std::{
+	collections::BTreeMap, path::PathBuf, error::Error, fmt::{Display, Formatter}
+};
 
 // Tracing library
-#[cfg(feature="tracing_output")]
-use tracing;
+//#[cfg(feature="tracing_output")]
+//use tracing;
 
 // UUID library
 use uuid;
@@ -49,7 +51,7 @@ impl Error for MergeError {}
 // Traits
 //
 
-pub trait Module where Self: Sized+Clone {}
+pub trait Module: Sized+Clone {}
 
 #[derive(Clone)]
 pub struct _DummyModule;
@@ -62,28 +64,7 @@ impl Module for _DummyModule {}
 // Structs
 //
 
-/// Represents a shader *environment* that, in essence, collects shader modules that should be accessible in conjunction
-/// with each other to shader programs that might want to use them.
-///
-/// Environments should be utilized the following way: *CGV-rs* crates that want to provide shaders should collect all
-/// their shaders either during compile-time (preferred) or at initialization, potentially already compiling them to
-/// some intermediate representation, and put each one into a [`Module`]. The modules collected this way should then go
-/// into an instance of [`Environment`] that the crate will make available through some means of its choosing. Since
-/// the modules the crate provides will typically also depend on modules from other crates (the most notable exception
-/// being the core `cgv` create, which only provides self-contained shader functionality), **this environment must be
-/// [merged](Environment::mergeWith) with the environments that the other crates provide their modules in** in order to
-/// make it usable in a self-contained manner\* (akin to static linking).
-///
-/// If the collected modules depend on some sort of compiler or preprocessor settings, some hash that uniquely
-/// identifies the required configuration should be set which will be checked when [merging](Environment::merge) and
-/// which crates using the environment can check to make sure that they are compatible. The crate exporting the
-/// environment should naturally provide a way for clients to find and use compatible settings.
-///
-/// ##### Footnotes
-///
-/// \*This is merely *CGV-rs* **convention**, in principle crates could require clients to merge in a list of stated
-/// dependencies on their side.
-#[derive(Clone)]
+/*#[derive(Clone)]
 pub struct Environment<ModuleType: Module> {
 	uuid: Uuid,
 	compatHash: u64,
@@ -257,6 +238,173 @@ impl<ModuleType: Module> Environment<ModuleType>
 		}
 		// - reference the merged-in environment itself
 		self.linkedEnvs.insert(other.uuid, other.ownedModules.clone());
+
+		// Done!
+		Ok(())
+	}
+}*/
+
+
+#[derive(Clone)]
+struct ModuleEntry<ModuleType: Module> {
+	module: ModuleType,
+	sourceEnv: Option<Uuid>
+}
+
+/// Represents a shader *environment* that, in essence, collects shader modules that should be accessible in conjunction
+/// with each other to shader programs that might want to use them.
+///
+/// Environments should be utilized the following way: *CGV-rs* crates that want to provide shaders should collect all
+/// their shaders either during compile-time (preferred) or at initialization, potentially already compiling them to
+/// some intermediate representation, and put each one into a [`Module`]. The modules collected this way should then go
+/// into an instance of [`Environment`] that the crate will make available through some means of its choosing. Since
+/// the modules the crate provides will typically also depend on modules from other crates (the most notable exception
+/// being the core `cgv` crate, which only provides self-contained shader functionality), **this environment must be
+/// [merged](Environment::mergeWith) with the environments that the other crates provide their modules in** in order to
+/// make it usable in a self-contained manner\* (akin to static linking).
+///
+/// If the collected modules depend on some sort of compiler or preprocessor settings, some hash that uniquely
+/// identifies the required configuration should be set which will be checked when [merging](Environment::merge) and
+/// which crates using the environment can check to make sure that they are compatible. The crate exporting the
+/// environment should naturally provide a way for clients to find and use compatible settings.
+///
+/// ##### Footnotes
+///
+/// \*This is merely *CGV-rs* **convention**, in principle crates could require clients to merge in a list of stated
+/// dependencies on their side.
+#[derive(Clone)]
+pub struct Environment<ModuleType: Module> {
+	uuid: Uuid,
+	label: &'static str,
+	compatHash: u64,
+	modules: BTreeMap<PathBuf, ModuleEntry<ModuleType>>,
+}
+impl<ModuleType: Module> Environment<ModuleType>
+{
+	/////
+	// Helper functions
+
+	/// Return a clone of `moduleEntry` where the source UUID is set to `self` _**iff**_ `moduleEntry`
+	/// does not have a source (i.e. it is an internal module).
+	#[inline(always)]
+	fn externalizeModule (&self, moduleEntry: &ModuleEntry<ModuleType>) -> ModuleEntry<ModuleType> {
+		ModuleEntry::<ModuleType> {
+			module: moduleEntry.module.clone(),
+			sourceEnv: if moduleEntry.sourceEnv.is_some() { moduleEntry.sourceEnv } else { Some(self.uuid) }
+		}
+	}
+
+	#[inline(always)]
+	fn externalizeModules (&self) -> BTreeMap<PathBuf, ModuleEntry<ModuleType>> {
+		self.modules.iter().map(
+			|(name, entry)| (
+				name.to_owned(), self.externalizeModule(entry)
+			)
+		).collect::<BTreeMap<_, _>>()
+	}
+
+	/// Construct an empty environment identified by the given
+	/// [UUID](https://de.wikipedia.org/wiki/Universally_Unique_Identifier).
+	pub fn withUuid (uuid: Uuid, label: &'static str) -> Self { Self {
+		uuid, label, compatHash: 0, modules: BTreeMap::new()
+	}}
+
+	/// Construct an empty environment identified by the given
+	/// [UUID](https://de.wikipedia.org/wiki/Universally_Unique_Identifier) and setting compatibility with the provided
+	/// [cgv_shader::]()
+	pub fn forContextWithUuid (context: &impl compile::Context<ModuleType>, uuid: Uuid, label: &'static str) -> Self { Self {
+		uuid, label, compatHash: context.environmentCompatHash(), modules: BTreeMap::new()
+	}}
+
+	/// Construct an empty environment identified by the given
+	/// [UUID](https://de.wikipedia.org/wiki/Universally_Unique_Identifier) and an initial compatibility hash.
+	pub fn withUuidAndCompatHash (uuid: Uuid, label: &'static str, compatHash: u64) -> Self { Self {
+		uuid, label, compatHash, modules: BTreeMap::new()
+	}}
+
+	/// Clones the current environment with a new UUID and label.
+	///
+	/// This function creates a new instance of the environment, retaining the compatibility hash and modules of the
+	/// current instance. However, **it will mark modules introduced by `self`** (i.e. those that
+	/// don't have a source environment) as **externally sourced** from the old UUID.
+	///
+	/// # Arguments
+	///
+	/// * `newUuid` – A `Uuid` representing the unique identifier for the new environment.
+	/// * `newLabel` – A static string slice representing the label for the new environment.
+	///
+	/// # Returns
+	///
+	/// A new instance of the environment with the provided UUID and label, with all modules and the same compatibility
+	/// hash as the original.
+	pub fn cloneWithNewUuid (&self, newUuid: Uuid, newLabel: &'static str) -> Self
+	{
+		// Commit all modules, marking the ones owned by `self` as being sourced from `self.uuid`
+		let newModules = self.externalizeModules();
+
+		// Build cloned environment
+		Self {
+			uuid: newUuid, label: newLabel, compatHash: self.compatHash, modules: newModules
+		}
+	}
+
+	/// Report the [UUID](https://de.wikipedia.org/wiki/Universally_Unique_Identifier) of this environment.
+	#[inline(always)]
+	pub fn uuid (&self) -> Uuid {
+		self.uuid
+	}
+
+	/// Reference the label string of this environment.
+	#[inline(always)]
+	pub fn label (&self) -> &'static str {
+		self.label
+	}
+
+	/// Report the compatibility hash of this environment.
+	#[inline(always)]
+	pub fn compatHash (&self) -> u64 {
+		self.compatHash
+	}
+
+	/// Merges another `Environment` into `self`.
+	///
+	/// # Arguments
+	///
+	/// * `other` – The other environment to merge with.
+	///
+	/// # Returns
+	///
+	/// Nothing in case of success, or a [`MergeError`] indicating what went wrong. In case of any error, `self`
+	/// **remains unchanged**!
+	pub fn mergeWith (&mut self, other: &Environment<ModuleType>) -> Result<(), MergeError>
+	{
+		// Initial sanity check
+		if self.compatHash != other.compatHash {
+			return Err(MergeError::Incompatible);
+		}
+
+		// Compile list of foreign modules to incorperate
+		let mut newModules = BTreeMap::<PathBuf, ModuleEntry<ModuleType>>::new();
+		for (path, moduleEntry) in &other.modules
+		{
+			// Decide what to do with this module
+			match self.modules.get(path)
+			{
+				None => {
+					// Case 1: this is a new module, include
+					let check = newModules.insert(
+						path.to_owned(), other.externalizeModule(moduleEntry)
+					);
+					assert!(check.is_none());
+				},
+
+				Some(ownEntry) => {
+					// Case 2: we already have a module at this path, and it has the same source: do not include
+					if ownEntry.sourceEnv != moduleEntry.sourceEnv {}
+					// Case 2: we already have a module at this path, and it has the same source: do not include
+				}
+			};
+		}
 
 		// Done!
 		Ok(())
