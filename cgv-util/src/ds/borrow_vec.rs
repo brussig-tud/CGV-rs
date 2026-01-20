8357bd2137319f812a06f4cc79fe03d924016191
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2026-01-20 14:50:30 +0100

    [util/ds] Sort out `BorrowVec` design and documentation

--- cgv-util/src/ds/borrow_vec.rs
+++ cgv-util/src/ds/borrow_vec.rs
@@ -14,9 +14,10 @@ use std::ops::Deref;
 // Structs
 //
 
-/// A zero-cost wrapper around [`Vec`] that dereferences to a slice of references to its element type. Very useful for
-/// convenience functions that create an array of things that require caller ownership, but which the owner will most
-/// typically just borrow to other functions (often as a slice of references).
+/// A zero-cost* convenience wrapper around [`Vec`] that [derefs](Deref) to a slice of references to the stored
+/// elements. Very useful in places where you need to call a function that needs such a slice of references. If you
+/// borrow this slice more than once, it naturally saves conversion costs without additional effort on your part as the
+/// slice of references is created only once [during construction](BorrowVec::new).
 ///
 /// # Example
 ///
@@ -28,10 +29,21 @@ use std::ops::Deref;
 ///         println!("worked on '{}'", string.as_ref());
 ///     }
 /// }
+/// fn workMoreOnStrings (strings: &[impl AsRef<str>]) {
+///     for string in strings {
+///         println!("worked some more on '{}'", string.as_ref());
+///     }
+/// }
 ///
 /// let strings = BorrowVec::new(vec!["foo".to_string(), "bar".into(), "baz".into()]);
-/// workOnStrings(strings.borrowed());
+/// workOnStrings(&*strings);
+/// workMoreOnStrings(&*strings); // <- saved one conversion
 /// ```
+///
+/// ##### Footnotes
+///
+/// \*It is zero-cost only if you actually use the slice of references it derefs to, otherwise you incur a small
+/// construction overhead that serves you no practical purpose.
 pub struct BorrowVec<'this, T: 'this> {
 	vec: Vec<T>,
 	borrowVec: Vec<&'this T>,
@@ -51,21 +63,24 @@ impl<'this, T: 'this> BorrowVec<'this, T>
 	pub fn new (vec: Vec<T>) -> Self {
 		let slice = unsafe {
 			// SAFETY: our struct will own this memory, so the references into said memory that it will keep cannot
-			//         outlive it (the lifetimes match). Also, `BorrowVec` hides all its fields inside the private
-			//         scope and defines no mutating methods, meaning Rust's aliasing rules are effectively never
-			//         violated.
+			//         outlive it (the lifetimes match). Also, the memory is on the heap and thus remains stable even
+			//         when the owning object `self.vec` is moved. Finally, `BorrowVec` hides all its fields inside the
+			//         private scope and defines no mutating methods, meaning Rust's aliasing rules are effectively
+			//         never violated.
 			&*(vec.as_slice() as *const [T])
 		};
 		Self { vec, borrowVec: slice.into_iter().map(|elem| elem).collect() }
 	}
 
 	/// Borrow a slice of the Vector of **owned** elements.
+	#[inline(always)]
 	pub fn owned (&self) -> &[T] {
 		self.vec.as_slice()
 	}
 
 	/// Obtain a slice of the Vector of **borrowed** elements.
-	pub fn borrowed (&self) -> &[&T] {
+	#[inline(always)]
+	pub fn borrowed (&self) -> &[&'this T] {
 		self.borrowVec.as_slice()
 	}
 }
@@ -80,6 +95,6 @@ impl<'this, T: 'this> Deref for BorrowVec<'this, T> {
 
 	#[inline(always)]
 	fn deref (&self) -> &Self::Target {
-		self.borrowVec.as_slice()
+		self.borrowed()
 	}
 }
