c2015f310a9773593fadc46243254baef0122594
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2026-03-29 17:18:22 +0200

    [cgv/gpu] Make `mipmap::Generator` trait expect stateful implementations

--- cgv/src/gpu/mod.rs
+++ cgv/src/gpu/mod.rs
@@ -36,34 +36,37 @@ use crate::*;
 
 /// Holds a reference to either a [render](wgpu::RenderPass) or [compute](wgpu::ComputePass) pass.
 #[derive(Debug)]
-pub enum Pass<'encoder> {
+pub enum Pass<'this> {
 	/// This GPU pass represents a "classical" render pass.
-	Render(wgpu::RenderPass<'encoder>),
+	Render(wgpu::RenderPass<'this>),
 
 	/// This GPU pass represents a compute pass.
-	Compute(wgpu::ComputePass<'encoder>)
+	Compute(wgpu::ComputePass<'this>)
 }
-impl<'encoder> Pass<'encoder>
+impl<'this> Pass<'this>
 {
 	/// Check if the GPU pass is a [render pass](Pass::Render).
-	fn _isRender (&self) -> bool {
+	#[expect(unused)]
+	fn isRender (&self) -> bool {
 		if let Pass::Render(_) = self { true } else { false }
 	}
 
 	/// Check if the GPU pass is a [compute pass](Pass::Compute).
-	fn _isCompute (&self) -> bool {
+	#[expect(unused)]
+	fn isCompute (&self) -> bool {
 		if let Pass::Render(_) = self { true } else { false }
 	}
 
 	/// Retrieve a reference to the wrapped [`wgpu::RenderPass`], panicking if we are not actually holding one.
-	fn _refRender (&mut self) -> &mut wgpu::RenderPass<'encoder> {
+	#[expect(unused)]
+	fn refRender<'outer> (&'outer mut self) -> &'outer mut wgpu::RenderPass<'this> {
 		if let Pass::Render(pass) = self { pass } else {
 			panic!("Attempted to reference render pass from non-render pass!");
 		}
 	}
 
 	/// Retrieve a reference to the wrapped [`wgpu::ComputePass`], panicking if we are not actually holding one.
-	fn refCompute (&mut self) -> &mut wgpu::ComputePass<'encoder> {
+	fn refCompute<'outer> (&'outer mut self) -> &'outer mut wgpu::ComputePass<'this> {
 		if let Pass::Compute(pass) = self { pass } else {
 			panic!("Attempted to reference compute pass from non-compute pass!");
 		}
