0a5c9d25d14d4ee5e37b6f411998ee559680f282
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2026-04-29 17:14:44 +0200

    [cgv/renderer] Make `GpuData` require `Send` and add host-equivalent marker traits

--- cgv/src/renderer/data/gpu/mod.rs
+++ cgv/src/renderer/data/gpu/mod.rs
@@ -18,7 +18,8 @@ use crate::{self as cgv, *};
 //
 
 /// Trait of GPU-side renderable data, ready for drawing by a [`Renderer`].
-pub trait Data {
+pub trait Data: Send
+{
 	/// Return the number of elements in the underlying data series.
 	fn num (&self) -> u32;
 
@@ -36,7 +37,8 @@ pub trait Data {
 
 /// Trait indicating that the render data contains index information; that is, indices into the data that connect
 /// individual data points to form complex primitives, like lines/line strips, triangles/triangle strips, etc.
-pub trait Indexed: Data {
+pub trait Indexed: Data
+{
 	/// Return the number of indices over the underlying data series.
 	fn numIndices (&self) -> u32;
 
@@ -46,3 +48,74 @@ pub trait Indexed: Data {
 	/// Return the [index format](wgpu::IndexFormat) the indices are stored in.
 	fn indexFormat (&self) -> wgpu::IndexFormat;
 }
+
+/// Marker trait for [`renderer::GpuData`] indicating that the data attributes are stored in an interleaved fashion (aka.
+/// "array of structs").
+pub trait Interleaved: Data {}
+
+/// Marker trait for [`renderer::GpuData`] indicating that the data attributes are stored in a non-interleaved fashion
+/// (aka. "struct of arrays").
+pub trait NonInterleaved: Data {}
+
+///
+pub trait CanHaveNormals: Data {
+	/// Indicate whether normals are available in the data.
+	fn hasNormals (&self) -> bool;
+}
+
+///
+pub trait HasNormals: CanHaveNormals {}
+
+///
+pub trait CanHaveTangents: Data {
+	/// Indicate whether tangents are available in the data.
+	fn hasTangents (&self) -> bool;
+}
+
+///
+pub trait HasTangents: CanHaveTangents {}
+
+///
+pub trait CanHaveRadii: Data {
+	/// Indicate whether radii are available in the data.
+	fn hasRadii (&self) -> bool;
+}
+
+///
+pub trait HasRadii: CanHaveRadii {}
+
+///
+pub trait CanHaveRadiusDerivs: CanHaveRadii {
+	/// Indicate whether radius derivatives are available in the data.
+	fn hasRadiusDerivs (&self) -> bool;
+}
+
+///
+pub trait HasRadiusDerivs: CanHaveRadiusDerivs+HasRadii {}
+
+///
+pub trait CanHaveOrientations: Data {
+	/// Indicate whether orientations are available in the data.
+	fn hasOrientations (&self) -> bool;
+}
+
+///
+pub trait HasOrientations: CanHaveOrientations {}
+
+///
+pub trait CanHaveScalings: Data {
+	/// Indicate whether scaling vectors are available in the data.
+	fn hasScalings (&self) -> bool;
+}
+
+///
+pub trait HasScalings: CanHaveScalings {}
+
+///
+pub trait CanHaveColors: Data {
+	/// Indicate whether colors are available in the data.
+	fn hasColors (&self) -> bool;
+}
+
+///
+pub trait HasColors: CanHaveColors {}
