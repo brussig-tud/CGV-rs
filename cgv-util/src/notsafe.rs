ce62beabcd126c312406fa03949e4d02e99ec943
Paul Hempel <paul.hempel@tu-dresden.de>
2026-05-18 12:20:03 +0200

    Remove unused unsafe functionality

--- cgv-util/src/notsafe.rs
+++ cgv-util/src/notsafe.rs
@@ -70,49 +70,6 @@ pub unsafe fn extendLifetime_mut<'out, Object> (object: &mut Object) -> &'out mu
 	unsafe { &mut *(object as *mut Object) }
 }
 
-/// Creates an (invalid if dereferenced) reference to an object of the specified type.
-///
-/// # Returns
-///
-/// A `'static` reference to some object of the specified type. Must not be dereferenced.
-#[inline(always)]
-pub const unsafe fn defaultRef<T> () -> &'static T {
-	unsafe { &*(1usize as *const T) }
-}
-
-/// Perform a shallow memory copy for copy-assigning from one value of type `T` to another. Can be used for copying
-/// objects that are not `Copy`.
-///
-/// # Arguments
-///
-/// * `target` – A reference to the target object that shall receive the copied memory contents.
-/// * `source` – A reference to the source object that holds the to-be-copied memory contents.
-#[inline(always)]
-pub unsafe fn copyAssign<T: Sized> (target: &mut T, source: &T) {
-	unsafe { std::ptr::copy_nonoverlapping(source as *const T, target as *mut T, 1); }
-}
-
-/// Construct a version of the given `str` slice "slid" to another memory location at `offset` bytes from its original
-/// place.
-///
-/// # Safety
-///
-/// The caller is responsible for ensuring that the memory at the new location contains valid UTF-8.
-///
-/// # Arguments
-///
-/// * `source` – The source `str` to slide.
-/// * `offset` – The delta (in bytes) to slide the `str` by.
-///
-/// # Returns
-///
-/// A new slice of the same length as `source` offset to the new location.
-#[inline(always)]
-pub unsafe fn offsetStr (source: &str, offset: isize) -> &str {
-	unsafe { str::from_utf8_unchecked(std::slice::from_raw_parts(source.as_ptr().offset(offset), source.len())) }
-}
-
-
 
 //////
 //
@@ -281,55 +238,4 @@ macro_rules! stridedRefIter
 	}};
 }
 pub use crate::stridedRefIter;
-
-
-/*////
-// Phony
-
-/// A container for holding a *phony* object of the given type.
-#[repr(align(8))]
-pub struct Phony<T: Sized> where [(); size_of::<T>()]: {
-	mem: [u8; size_of::<T>()]
-}
-impl<T: Sized> Phony<T> where [(); size_of::<T>()]: {
-	/// Allocate memory to hold the phony object.
-	pub unsafe fn new() -> Self { Phony {
-		mem: [0; size_of::<T>()]
-	}}
-}
-impl<T: Sized> AsRef<T> for Phony<T> where [(); size_of::<T>()]: {
-	#[inline(always)]
-	fn as_ref (&self) -> &T {
-		unsafe { &*(&self.mem as *const u8 as *const T) }
-	}
-}
-impl<T: Sized> AsMut<T> for Phony<T> where [(); size_of::<T>()]: {
-	#[inline(always)]
-	fn as_mut (&mut self) -> &mut T {
-		unsafe { &mut *(&mut self.mem as *mut u8 as *mut T) }
-	}
-}
-impl<T: Sized> Borrow<T> for Phony<T> where [(); size_of::<T>()]: {
-	#[inline(always)]
-	fn borrow (&self) -> &T {
-		self.as_ref()
-	}
-}
-impl<T: Sized> BorrowMut<T> for Phony<T> where [(); size_of::<T>()]: {
-	#[inline(always)]
-	fn borrow_mut (&mut self) -> &mut T {
-		self.as_mut()
-	}
-}
-impl<T: Sized> Deref for Phony<T> where [(); size_of::<T>()]: {
-	type Target = T;
-	fn deref (&self) -> &Self::Target {
-		self.as_ref()
-	}
-}
-impl<T: Sized> DerefMut for Phony<T> where [(); size_of::<T>()]: {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		self.as_mut()
-	}
-}*/
 
